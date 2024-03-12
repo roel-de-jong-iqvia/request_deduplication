832fb13051090ea2aef615bda9729b0ca894e733
roel-de-jong-iqvia <roel.jong@iqvia.com>
2024-03-12 20:48:48 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,6 @@ import { QueryService } from "./query.service";
   template: `
     <div>
       <button (click)="invalidate()">Invalidate Query</button>
-      <ng-container *ngIf="characters$ | async as characters">
-        <p *ngIf="characters.status === 'loading'">Loading...</p>
-        <p *ngIf="characters.status === 'error'">Error :(</p>
-        <ng-container *ngIf="characters.status === 'success'">
-          <article *ngFor="let person of characters.data">
-            {{ person.name }} - {{ person.gender }}: {{ person.species }}
-          </article>
-        </ng-container>
-      </ng-container>
       <custom-dialog [event]="reload"></custom-dialog>
       <custom-dialog [event]="reload"></custom-dialog>
       <custom-dialog [event]="reload"></custom-dialog>
@@ -26,19 +17,10 @@ import { QueryService } from "./query.service";
     </div>
   `,
 })
-export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  query = injectQuery();
+export class AppComponent {
   queryClient = injectQueryClient();
   queryService = inject(QueryService);
   reload = new EventEmitter();
-
-  showDialog = false;
-  characters$: Observable<any>;
-
-  ngOnInit() {
-    this.characters$ = this.queryService.character$("joehoe").pipe(take(1));
-  }
 
   invalidate() {
     this.queryService.invalidateQueries("joehoe");
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { platformBrowser } from "@angular/platform-browser";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
+import 'zone.js';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
+platformBrowser()
   .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
